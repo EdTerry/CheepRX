f674cd38831943335dca1f4f41d44cf08586dabd
JeffAntoine <Janto031@fiu.edu>
2018-02-03 14:28:16 -0500

    changes

---
+++ src/server/server.py
@@ -0,0 +1,37 @@
+import requests
+import sys, threading
+from bs4 import BeautifulSoup, SoupStrainer
+import lxml
+
+originalPrice =""
+discountPrice =""
+medication ="adderall"
+store =""
+distance =""
+url=""
+
+
+#class crawl_goodrx():    
+
+def crawl_pages():
+    global originalPrice
+    global discountPrice
+    global store
+    global url
+
+    url = "https://www.goodrx.com/" + medication + "?drug-name=" + medication
+    sourceCode = requests.get(url)
+    plainText = sourceCode.text
+    strainer = SoupStrainer('div', { 'event-id' : 'priceRow'})
+    soup = BeautifulSoup(plainText, "lxml", parse_only=strainer)
+    originalPrice = soup.find(class_ ="est-price").string
+    discountPrice = soup.find(class_ ="drug-price")
+    store=soup.find(class_ ="store-name")
+
+    print(originalPrice)
+    print(discountPrice)
+    print(store)
+        
+
+if __name__ == "__main__":
+    goodrx = crawl_pages()
